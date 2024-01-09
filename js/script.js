120837f398d2a12b77002968e71d49c7b648e07f
heeseung <love01040701320@gmail.com>
2024-01-09 12:46:46 +0900

    모달창 작업 완성

--- js/script.js
+++ js/script.js
@@ -68,4 +68,12 @@ window.onload = function () {
       topBtnImg.classList.remove("up");
     }
   });
+
+  // 안내창 스크립트
+  const body = document.querySelector("body");
+  const modal = document.querySelector(".modal-wrap");
+  modal.addEventListener("click", function () {
+    modal.style.display = "none";
+    fadeout(modal);
+  });
 };
